5581c4a6b4d27d6995d5b729e34bdb44cf6ec33c
Archisha Bhattacharya <aba406@uregina.ca>
2023-10-30 00:43:09 -0600

    Update SignUp.js

--- 02. Project Developement/a. Testing/Front-end/visuspeak/src/App.js
+++ 02. Project Developement/a. Testing/Front-end/visuspeak/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Header from './components/Header';
 import Home from "./components/Home.js";
 import Login from './components/Login';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <>
       <Header />
-      <Login />
+      <SignUp />
     </>
   );
 }
--- 02. Project Developement/a. Testing/Front-end/visuspeak/src/components/SignUp.js
+++ 02. Project Developement/a. Testing/Front-end/visuspeak/src/components/SignUp.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function Login() {
+export default function SignUp() {
   return (
     <div class="container my-5 ms-5">
-      <h1 className="text-center">Login</h1>
+      <h1 className="text-center">Sign Up</h1>
       <div class="container text-right">
         <div class="row">
           <div class="col"></div>
@@ -11,10 +11,26 @@ export default function Login() {
             <form class="container my-5 ms-5 text-end">
               <div class="row mb-4">
                 <label for="inputEmail3" class="col-sm-3 col-form-label">
-                  Username or Email:
+                  First Name:
                 </label>
                 <div class="col-sm-7">
                   <input type="text" class="form-control" id="inputEmail3" />
+                </div>
+              </div>
+              <div class="row mb-4">
+                <label for="inputEmail3" class="col-sm-3 col-form-label">
+                  Last Name:
+                </label>
+                <div class="col-sm-7">
+                  <input type="text" class="form-control" id="inputEmail3" />
+                </div>
+              </div>
+              <div class="row mb-4">
+                <label for="inputEmail3" class="col-sm-3 col-form-label">
+                  Email:
+                </label>
+                <div class="col-sm-7">
+                  <input type="email" class="form-control" id="inputEmail3" />
                 </div>
               </div>
               <div class="row mb-4">
@@ -30,6 +46,31 @@ export default function Login() {
                 </div>
               </div>
               <div class="row mb-4">
+                <label for="inputPassword3" class="col-sm-3 col-form-label">
+                  Confirm Password:
+                </label>
+                <div class="col-sm-7">
+                  <input
+                    type="password"
+                    class="form-control"
+                    id="inputPassword3"
+                  />
+                </div>
+              </div>
+              <div class="row mb-4 text-start">
+              <label class="col-sm-3 col-form-label"></label>
+                <div class="col-sm-8 form-check">
+                  <input
+                    type="checkbox"
+                    class="form-check-input"
+                    id="exampleCheck1"
+                  />
+                  <label class="form-check-label" for="exampleCheck1">
+                    Do you agree with the <a href="/">Terms and Conditions</a>
+                  </label>
+                </div>
+              </div>
+              <div class="row mb-4">
                 <label
                   for="inputPassword3"
                   class="col-sm-3 col-form-label"
@@ -37,7 +78,7 @@ export default function Login() {
                 <div class="col-sm-7">
                   <div className="d-grid gap-2 d-md-flex justify-content-md-end">
                     <button type="submit" class="btn btn-dark btn-lg">
-                      Login
+                      Sign Up
                     </button>
                   </div>
                 </div>
@@ -49,7 +90,7 @@ export default function Login() {
                 ></label>
                 <div class="col-sm-7">
                   <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-                    <a href="/">Click here to sign up</a>
+                    <a href="/">Click here to log in</a>
                   </div>
                 </div>
               </div>
