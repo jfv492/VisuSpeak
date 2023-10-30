42c8260fc0012f5bf951af422787cd9b16c7a8b4
Archisha Bhattacharya <aba406@uregina.ca>
2023-10-30 00:27:35 -0600

    update Login.js

--- 02. Project Developement/a. Testing/Front-end/visuspeak/src/App.js
+++ 02. Project Developement/a. Testing/Front-end/visuspeak/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from './components/Header';
 import Home from "./components/Home.js";
+import Login from './components/Login';
 
 function App() {
   return (
     <>
       <Header />
-      <Home />
+      <Login />
     </>
   );
 }
---
+++ 02. Project Developement/a. Testing/Front-end/visuspeak/src/components/Login.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <div class="container my-5 ms-5">
+      <h1 className="text-center">Login</h1>
+      <div class="container text-right">
+        <div class="row">
+          <div class="col"></div>
+          <div class="col-10">
+            <form class="container my-5 ms-5">
+              <div class="row mb-4">
+                <label for="inputEmail3" class="col-sm-3 col-form-label">
+                  Username or Email:
+                </label>
+                <div class="col-sm-7">
+                  <input type="email" class="form-control" id="inputEmail3" />
+                </div>
+              </div>
+              <div class="row mb-4">
+                <label for="inputPassword3" class="col-sm-3 col-form-label">
+                  Password:
+                </label>
+                <div class="col-sm-7">
+                  <input
+                    type="password"
+                    class="form-control"
+                    id="inputPassword3"
+                  />
+                </div>
+              </div>
+              <div class="row mb-4">
+                <label
+                  for="inputPassword3"
+                  class="col-sm-3 col-form-label"
+                ></label>
+                <div class="col-sm-7">
+                  <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+                    <a href="/">Click here to sign up</a>
+                  </div>
+                </div>
+              </div>
+              <div class="row mb-4">
+                <label
+                  for="inputPassword3"
+                  class="col-sm-3 col-form-label"
+                ></label>
+                <div class="col-sm-7">
+                  <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+                    <button type="submit" class="btn btn-dark btn-lg">
+                      Login
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="col"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
