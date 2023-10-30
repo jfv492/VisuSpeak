70e83197e523e1213b0741396848c6be1623d932
Archisha Bhattacharya <aba406@uregina.ca>
2023-10-30 01:16:07 -0600

    Update Footer.js

--- 02. Project Developement/a. Testing/Front-end/visuspeak/src/components/Footer.js
+++ 02. Project Developement/a. Testing/Front-end/visuspeak/src/components/Footer.js
@@ -1,4 +1,4 @@
-import FooterLogo from "../images/VisuSpeak_Logo_White.png";
+import FooterLogo from "../images/White.png";
 import "../VisuSpeak.css";
 import React from "react";
 
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <div className="text-center">
       <footer className="d-flex flex-wrap justify-content-between align-items-center border-top">
-        <div className="col d-flex align-items-center">
+        <div className="col d-flex align-items-center ms-4">
           <a
             href="/"
             className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1"
