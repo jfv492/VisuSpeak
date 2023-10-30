8cb9f75db48e263e7ca9c3cf249952f9353fc1d9
Archisha Bhattacharya <aba406@uregina.ca>
2023-10-30 00:46:45 -0600

    Add Routes from 'react-router-dom'

--- 02. Project Developement/a. Testing/Front-end/visuspeak/src/App.js
+++ 02. Project Developement/a. Testing/Front-end/visuspeak/src/App.js
@@ -1,14 +1,27 @@
-import './App.css';
-import Header from './components/Header';
+import "./App.css";
+import Header from "./components/Header";
 import Home from "./components/Home.js";
-import Login from './components/Login';
-import SignUp from './components/SignUp';
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Header />
-      <SignUp />
+      {/* <Header />
+      <SignUp /> */}
+      <BrowserRouter>
+        {" "}
+        {/* renamed from Router to BrowserRouter */}
+        <Header />
+        <Home />
+        <div>
+          <Routes>
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/signup" element={<SignUp />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
