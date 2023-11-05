9467b13663f39ecba700bfa5e0850e3e535a56ea
Jasmeet Singh <jasmeet.singh29@outlook.com>
2023-11-05 13:02:23 -0600

    Update Back-end

--- 02. Project Developement/a. Testing/Front-end/visuspeak/server/index.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-const app = express();
-const PORT = process.env.PORT || 3030;
-
-app.listen(PORT, function () {
-  console.log(`Server is running on port ${PORT}`);
-});
