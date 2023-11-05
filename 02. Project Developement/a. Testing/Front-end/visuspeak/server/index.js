1bf8a116c4d3cdea4ffed7bf83cb2e52c3e96505
Archisha Bhattacharya <aba406@uregina.ca>
2023-11-05 12:22:11 -0600

    added port number for backend

--- 02. Project Developement/a. Testing/Front-end/visuspeak/server/index.js
+++ 02. Project Developement/a. Testing/Front-end/visuspeak/server/index.js
@@ -1,55 +1,7 @@
-import mysql from "mysql";
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 3030;
 
-import inquirer from "inquirer";
-
-const mysql_user = {
-  host: "localhost",
-  user: "root",
-  password: "VisuSpeak2023root",
-};
-
-const connection = mysql.createConnection(mysql_user, {
-  multipleStatements: true,
+app.listen(PORT, function () {
+  console.log(`Server is running on port ${PORT}`);
 });
-
-function query(query) {
-  connection.query(query, (error, result) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(result);
-    }
-    inquire();
-  });
-}
-
-function inquire() {
-  inquirer
-    .prompt([
-      {
-        name: "statement",
-        message: "mysql>",
-      },
-    ])
-    .then((answer) => {
-      if (answer.statement === "quit") {
-        console.log("Disconnected and exit");
-        connection.end();
-      } else {
-        query(answer.statement);
-      }
-    });
-}
-
-function connect() {
-  connection.connect((error) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Connected to SQL");
-      inquire();
-    }
-  });
-}
-
-connect();
