0ee56f032045c93b25dcf20e78b04c20dac8e334
Jasmeet Singh <jasmeet.singh29@outlook.com>
2023-11-05 13:38:29 -0600

    Connected MySQL db

--- 02. Project Developement/a. Testing/Front-end/visuspeak/server/server.js
+++ 02. Project Developement/a. Testing/Front-end/visuspeak/server/server.js
@@ -5,10 +5,26 @@ const cors = require('cors')
 const app = express ()
 app.use(cors())
 
+const db = mysql.createConnection({
+    host: "localhost",
+    user: 'root',
+    password: '',
+    database: 'VisuSpeak'
+})
+
 app.get('/', (re, res)=> {
     return res.json("From Backend VisuSpeak");
 })
 
-app.listen(8081, () => {
-    console.log("listening");
+app.get('/users', (req, res)=>{
+    const sql = "SELECT * FROM user";
+    db.query(sql, (err, data)=>{
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
 })
