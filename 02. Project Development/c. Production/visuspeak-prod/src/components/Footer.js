447813241d22f7bb709aabb6819359d66694e1d7
Archisha Bhattacharya <aba406@uregina.ca>
2023-11-11 21:53:56 -0600

    add production code for front-end

---
+++ 02. Project Development/c. Production/visuspeak-prod/src/components/Footer.js
@@ -0,0 +1,16 @@
+import FooterLogo from "../images/White.png";
+import "../VisuSpeak.css";
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="text-center">
+      <footer className="d-flex flex-wrap justify-content-between align-items-center border-top">
+        <div className="col d-flex align-items-center">
+            <img src={FooterLogo} alt="VisuSpeak Logo" className="mx-5"  height="50" />
+          <p className="mb-3 mb-md-0">© 2023 Team VisuSpeak</p>
+        </div>
+      </footer>
+    </div>
+  )
+}
