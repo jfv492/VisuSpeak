7130036d181f471c2078f9102c1a35a98838a1c9
Archisha Bhattacharya <aba406@uregina.ca>
2023-11-11 22:02:16 -0600

    add server setup for localhost

---
+++ 02. Project Development/b. Testing/Front-end/visuspeak/server/db.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'VisuSpeak',
+});
+
+
+db.connect((err) => {
+  if (err) {
+    console.error('Database connection failed: ' + err.message);
+    return;
+  }
+  console.log('Connected to the MySQL database');
+});
+
+module.exports = db;
