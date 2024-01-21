7e645db046779a0974d2153003e88530973df938
Jasmeet Singh <jasmeet.singh29@outlook.com>
2024-01-21 17:46:01 -0600

    Updated Home.js

--- 02. Project Development/b. Testing/Front-end/visuspeak/src/components/Home.js
+++ 02. Project Development/b. Testing/Front-end/visuspeak/src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import StockImage from "../images/FillerPhoto.jpg";
 import { Link, useLocation } from "react-router-dom";
+import Background from "./Background.js";
 
 export default function Home() {
   let location = useLocation();
@@ -43,65 +44,10 @@ export default function Home() {
                     location.pathname === "/about" ? "d-none" : ""
                   }`}>
       </div>
-      <div className="container my-5">
-          <div className="row mt-5">
-            <div className="col large-text-style">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Volutpat ac tincidunt vitae semper quis lectus nulla at. Arcu
-              dictum varius duis at. Arcu non odio euismod lacinia at quis risus
-              sed. Mollis nunc sed id semper risus in. Aliquam sem fringilla ut
-              morbi tincidunt augue interdum velit euismod. Lectus mauris
-              ultrices eros in cursus. Nullam vehicula ipsum a arcu cursus vitae
-              congue mauris rhoncus. Ultrices gravida dictum fusce ut placerat
-              orci nulla. Diam maecenas ultricies mi eget mauris pharetra et.
-              Sem nulla pharetra diam sit amet nisl suscipit. Platea dictumst
-              quisque sagittis purus sit amet volutpat consequat. Cursus in hac
-              habitasse platea dictumst. Morbi enim nunc faucibus a.
-            </div>
-            <div className="col">
-              <img src={StockImage} className="rounded mx-auto d-block" alt="..." height="350"/>
-            </div>
-          </div>
-          <div className="row mt-5">
-          <div className="col">
-              <img src={StockImage} className="rounded mx-auto d-block" alt="..." height="350"/>
-            </div>
-            <div className="col large-text-style">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Volutpat ac tincidunt vitae semper quis lectus nulla at. Arcu
-              dictum varius duis at. Arcu non odio euismod lacinia at quis risus
-              sed. Mollis nunc sed id semper risus in. Aliquam sem fringilla ut
-              morbi tincidunt augue interdum velit euismod. Lectus mauris
-              ultrices eros in cursus. Nullam vehicula ipsum a arcu cursus vitae
-              congue mauris rhoncus. Ultrices gravida dictum fusce ut placerat
-              orci nulla. Diam maecenas ultricies mi eget mauris pharetra et.
-              Sem nulla pharetra diam sit amet nisl suscipit. Platea dictumst
-              quisque sagittis purus sit amet volutpat consequat. Cursus in hac
-              habitasse platea dictumst. Morbi enim nunc faucibus a.
-            </div>
-          </div>
-          <div className="row mt-5">
-            <div className="col large-text-style">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Volutpat ac tincidunt vitae semper quis lectus nulla at. Arcu
-              dictum varius duis at. Arcu non odio euismod lacinia at quis risus
-              sed. Mollis nunc sed id semper risus in. Aliquam sem fringilla ut
-              morbi tincidunt augue interdum velit euismod. Lectus mauris
-              ultrices eros in cursus. Nullam vehicula ipsum a arcu cursus vitae
-              congue mauris rhoncus. Ultrices gravida dictum fusce ut placerat
-              orci nulla. Diam maecenas ultricies mi eget mauris pharetra et.
-              Sem nulla pharetra diam sit amet nisl suscipit. Platea dictumst
-              quisque sagittis purus sit amet volutpat consequat. Cursus in hac
-              habitasse platea dictumst. Morbi enim nunc faucibus a.
-            </div>
-            <div className="col">
-              <img src={StockImage} className="rounded mx-auto d-block" alt="..." height="350"/>
-            </div>
-          </div>
+      <div>
+          <Background />
       </div>
+    
     </>
   );
 }
