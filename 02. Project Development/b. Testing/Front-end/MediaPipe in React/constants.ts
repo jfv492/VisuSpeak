9e1c51f5c37a39bfe60c090dc98934d9ebf17b73
Archisha Bhattacharya <aba406@uregina.ca>
2024-01-21 17:12:36 -0600

    Deployed MediaPipe model in React

---
+++ 02. Project Development/b. Testing/Front-end/MediaPipe in React/constants.ts
@@ -0,0 +1,9 @@
+const prefix = 'http://localhost:3001';
+
+export { prefix };
+
+const CONFIGS = {
+  keypointClassifierLabels: ['Hello', 'Yes', 'No', 'Please', 'Thank You'],
+};
+
+export default CONFIGS;
