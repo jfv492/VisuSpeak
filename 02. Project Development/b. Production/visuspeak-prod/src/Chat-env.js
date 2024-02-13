e8c8df10414bfbfb01f7f48de0176a87a40ce7d2
Archisha Bhattacharya <aba406@uregina.ca>
2024-02-12 19:04:29 -0600

    update .gitignore

--- 02. Project Development/b. Production/visuspeak-prod/src/Chat-env.js
+++
@@ -1,3 +0,0 @@
-const modelChatUrl = `https://visuspeak-chat-server-2c8ad7bff3cf.herokuapp.com`;
-
-export default modelChatUrl;
--- 02. Project Development/b. Production/visuspeak-prod/src/Server-env.js
+++
@@ -1,4 +0,0 @@
-const PORT = process.env.PORT || 3306;
-const serverUrl = `https://visuspeak-server-167ef4c9d254.herokuapp.com`;
-
-export default serverUrl;
