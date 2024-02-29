ea6bb184e32d9ee55b8f21736b147ec9d5bd9e27
Archisha Bhattacharya <aba406@uregina.ca>
2024-02-28 22:12:10 -0600

    update firebase.js

--- 02. Project Development/a. Testing/Front-end/visuspeak/src/firebase.js
+++ 02. Project Development/a. Testing/Front-end/visuspeak/src/firebase.js
@@ -7,13 +7,14 @@ import { getDatabase } from "firebase/database";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
+  databaseURL:  process.env.REACT_APP_databaseURL,
   projectId: process.env.REACT_APP_projectId,
   storageBucket: process.env.REACT_APP_storageBucket,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
   appId: process.env.REACT_APP_appId
 };
 
-
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const storage = getStorage();
